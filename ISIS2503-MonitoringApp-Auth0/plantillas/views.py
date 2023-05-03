87b7cfdd5f1911a7865441d6f5f623b337fa2370
Daniela Uribe <d.uribe1@uniandes.edu.co>
2023-05-02 19:15:34 -0500

    Permiso añadido

--- ISIS2503-MonitoringApp-Auth0/plantillas/views.py
+++ ISIS2503-MonitoringApp-Auth0/plantillas/views.py
@@ -1,33 +1,42 @@
 from django.shortcuts import render
 from .forms import PlantillaForm
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from .logic.logic_plantilla import create_plantilla, get_plantillas
 from django.contrib.auth.decorators import login_required
+from windmy.auth0backend import getRole
 
 @login_required
 def plantilla_list(request):
-    plantillas = get_plantillas()
-    context = {
-        'plantilla_list': plantillas
-    }
-    return render(request, 'Plantilla/plantillas.html', context)
+    role = getRole(request)
+    if role == "Doctor" or role=="Enfermero":
+        plantillas = get_plantillas()
+        context = {
+            'plantilla_list': plantillas
+        }
+        return render(request, 'Plantilla/plantillas.html', context)
+    else:
+        return HttpResponse("Unauthorized User")
 
 def plantilla_create(request):
-    if request.method == 'POST':
-        form = PlantillaForm(request.POST)
-        if form.is_valid():
-            create_plantilla(form)
-            messages.add_message(request, messages.SUCCESS, 'Plantilla create successful')
-            return HttpResponseRedirect(reverse('plantillaCreate'))
+    role = getRole(request)
+    if role == "Doctor" or role=="Enfermero":
+        if request.method == 'POST':
+            form = PlantillaForm(request.POST)
+            if form.is_valid():
+                create_plantilla(form)
+                messages.add_message(request, messages.SUCCESS, 'Plantilla create successful')
+                return HttpResponseRedirect(reverse('plantillaCreate'))
+            else:
+                print(form.errors)
         else:
-            print(form.errors)
+            form = PlantillaForm()
+
+        context = {
+            'form': form,
+        }
+
+        return render(request, 'Plantilla/plantillaCreate.html', context)
     else:
-        form = PlantillaForm()
-
-    context = {
-        'form': form,
-    }
-
-    return render(request, 'Plantilla/plantillaCreate.html', context)
+        return HttpResponse("Unauthorized User")
